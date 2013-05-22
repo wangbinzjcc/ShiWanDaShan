d2b6896bb577fb3ccf7bf9f5842ecd33d090b530
wangbinzjcc <wangbin2010zjcc@yahoo.cn>
2013-05-23 00:38:55 +0800

    2013-5-23 0:38:52

--- ShiWanDataUnique.r
+++ ShiWanDataUnique.r
@@ -1,3 +1,4 @@
+######
 setwd('F:\\DataW\\shiwan-data')
 dir()
 ShiWdata <- read.csv("shiwanData2013-5-15.csv")
@@ -6,13 +7,9 @@ uniq.dat <- with(ShiWdata, tapply(鉴定名,样方号,unique))
 
 uniq.dat00 <- unique(unlist(uniq.dat))
 c00 <- unlist(uniq.dat)
-mode(c00)
-class(c00)
 c01 <- uniq.dat00
-mode(c01)
-class(c01)
-names(c00)
 names(c01) <- 1:length(c01)
 c02 <- c(as.character(c00), as.character(c01))
 names(c02) <- c(names(c00), 1:length(c01))
 write.csv(c02, 'uniq.dat00.csv')
+##############
